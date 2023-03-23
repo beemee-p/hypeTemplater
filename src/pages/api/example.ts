334e6af64d46b61f86439ec6435069e73ee39097
Yuna Park <una@stunning.kr>
2023-03-23 21:17:18 +0900

    implement: firebas api

--- src/pages/api/example.ts
+++ src/pages/api/example.ts
@@ -1,3 +1,14 @@
+import { collection, getDocs, Firestore } from "firebase/firestore/lite";
+import { db } from "./firebase/ClientApp";
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  // Get a list of cities from your database
+  async function getCities(db: Firestore) {
+    const portfolioCol = collection(db, "portfolio");
+    const portfolioSnapshot = await getDocs(portfolioCol);
+    const portfolioList = portfolioSnapshot.docs.map((doc) => doc.data());
+    return portfolioList;
+  }
+  const data = await getCities(db);
+  return new Response(JSON.stringify(data));
 }
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,7 +1,18 @@
 import { ReactElement } from "react";
 
-const PortfolioList = (): ReactElement => {
-  return <div>PortfolioList</div>;
+const PortfolioList = ({ data }: any): ReactElement => {
+  console.log(data.title);
+  return <div>{data.title}</div>;
 };
 
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`http://localhost:3000/api/example.ts`);
+  console.log(res);
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
 export default PortfolioList;

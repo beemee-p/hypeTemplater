1eae61c156535a571c31a7dae001093e5c35454b
Yuna Park <una@stunning.kr>
2023-03-23 20:10:01 +0900

    setup: page routes

---
+++ src/pages/edit/index.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+const PortfolioEdit = (): ReactElement => {
+  return <div>PortfolioEdit</div>;
+};
+
+export default PortfolioEdit;
---
+++ src/pages/list/index.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+const PortfolioList = (): ReactElement => {
+  return <div>PortfolioList</div>;
+};
+
+export default PortfolioList;

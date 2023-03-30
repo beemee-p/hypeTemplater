862bfc3dfaa850a4c4058aa4122f643f6f6eb91d
Lee-ji-soo <nbsp3@naver.com>
2023-03-31 02:04:03 +0900

    update portfolio list api

--- src/pages/api/portfolios/index.ts
+++ src/pages/api/portfolios/index.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import {
   collection,
   doc,
@@ -8,14 +9,43 @@ import {
 import { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../firebase/ClientApp";
 
+=======
+import { collection, getDoc, getDocs, Firestore } from 'firebase/firestore';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../firebase/ClientApp';
+>>>>>>> update portfolio list api
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   const getPortfolioList = async (db: Firestore) => {
     const portfolioCol = collection(db, "portfolio");
     const portfolioSnapshot = await getDocs(portfolioCol);
-    const portfolioList = portfolioSnapshot.docs.map((doc) => doc.data());
+    const portfolioList = await Promise.all(
+      portfolioSnapshot.docs.map(async (doc) => {
+        const portfolio = doc.data();
+
+        if (portfolio.thumbnail) {
+          const thumbnailSnapShot = await getDoc(portfolio.thumbnail);
+          portfolio.thumbnail = thumbnailSnapShot.data();
+        }
+
+        if (portfolio.user) {
+          const userSnapShot = await getDoc(portfolio.user);
+          portfolio.user = userSnapShot.data();
+        }
+
+        if (portfolio.comment) {
+          const commentSnapShot = await getDoc(portfolio.comment);
+          portfolio.comment = commentSnapShot.data();
+        }
+
+        return portfolio;
+      }),
+    );
     return portfolioList;
   };
 
   const data = await getPortfolioList(db);
+  await console.log({ data });
   res.status(200).json(data);
 }
+
+//https://velog.io/@minsangk/2019-09-06-0209-%EC%9E%91%EC%84%B1%EB%90%A8-eik06xy8mm
--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,8 +1,39 @@
+<<<<<<< HEAD
 import { ReactElement } from "react";
+=======
+import { ReactElement } from 'react';
+import styled from 'styled-components';
+import { Portfolio } from '@/model/Portfolio';
+>>>>>>> update portfolio list api
 
-const PortfolioList = (props: any): ReactElement => {
-  return <div>{props.portfolios.map((i: any) => i.title)}</div>;
+interface PortfolioListProps {
+  portfolios: Portfolio[];
+}
+const PortfolioList = (props: PortfolioListProps): ReactElement => {
+  return (
+    <section>
+      <h1>LIST PAGE</h1>
+      {/* TODO:GRID Component*/}
+      {props.portfolios.map((i: Portfolio) => (
+        <ARTICLE_Card>
+          <img src={i.thumbnail?.url} alt={i.thumbnail.title} />
+          <h3>{i.title}</h3>
+          <p>{i.user.name}</p>
+        </ARTICLE_Card>
+      ))}
+    </section>
+  );
 };
+
+const ARTICLE_Card = styled.div`
+  //TODO:aspect RATIO
+  img {
+    width: 250px;
+    height: 250px;
+    object-fit: cover;
+    border-radius: 4px;
+  }
+`;
 
 export async function getServerSideProps() {
   try {
